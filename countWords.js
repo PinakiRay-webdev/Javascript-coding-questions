eaa265dd4397f141e3c819fe5fc9773f09c3edd8
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2025-02-08 02:28:09 +0530

    added enhanced method for problem countWords

--- countWords.js
+++ countWords.js
@@ -12,5 +12,14 @@ function countWords(str){
     return count;
 }
 
+//enhanced method to count words in a string
+function enchancedCountWords(str){
+    str = str.trim();  
+    const words = str.split(' ');
+
+    return words.length;
+}
+
 const str = " Odisha is India's best kept secret "
 console.log(`Total word count is ${countWords(str) + 1}`)
+console.log(`Total word count is ${enchancedCountWords(str)}`)
